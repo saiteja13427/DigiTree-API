dded0c358ed843e2a55bb9b24dc24224842cbcdd
saiteja13427 <saiteja13427@gmail.com>
2021-10-20 14:37:57 +0530

    Rate Limit Correction

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.use(hpp());
 
 //Rate limit
 const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 15 minutes
+  windowMs: 10 * 60 * 1000, // 10 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 });
 
